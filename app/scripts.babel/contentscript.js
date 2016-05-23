b2c8dbc6a7538fb242dd9d5eaba6522a0c910b35
Nicolas Roos <nicolas.k.roos@gmail.com>
2016-05-23 21:31:20 +0200

    improved error handling

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -4,17 +4,20 @@ var extensionId = 'ednklkkohnjkdfniolbmafkgnjkmchjg';
 
 var links = document.links;
 
-
 for(var i=0; i < links.length; i++) {
   if(links[i].href && links[i].href.indexOf('magnet') === 0) {
     let magnetLink = links[i].href;
     links[i].addEventListener('click', function () {
-      console.log('MagnetLink',magnetLink);
       chrome.runtime.sendMessage(
         extensionId,
         { method: 'setClipboard', value: magnetLink },
         function(response) {
-          console.log('extension setClipboard response', response);
+          if(!response) {
+            console.error('Error. Could not copy to clipboard');
+          } else {
+            console.log('Copied magnet link to clipboard');
+          }
+
         }
       );
     })
